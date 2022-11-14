7fe3748fe8542278df8cbde71890d60f686fced1
Azda27 <growtopiagame875236@gmail.com>
2022-11-15 06:30:15 +0800

    Commit

--- admin/data.php
+++ admin/data.php
@@ -2,7 +2,6 @@
 include 'session.php';
 $result = mysqli_query($conn, "SELECT * FROM admin");
 $row = mysqli_fetch_assoc($result);
-$no = 1;$data = mysqli_query($conn,"select * from link");while($d = mysqli_fetch_array($data)){
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,10 +11,11 @@ $no = 1;$data = mysqli_query($conn,"select * from link");while($d = mysqli_fetch
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 	<title>Data</title>
-	<link rel="stylesheet" href="rTVUG+j.css">
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-
+	<div>
+		
 	<h2>Data Link</h2>
 	<br/>
 	<a href="input">Input link</a> 
@@ -24,7 +24,12 @@ $no = 1;$data = mysqli_query($conn,"select * from link");while($d = mysqli_fetch
 	<br/><br><br>
 	<h1>Welcome <?php echo $row["username"]; ?></h1>
 	<br><br>
+	</div>
 	<table>
+		<tr>
+			<th colspan="4" scope="colgroup"></th>
+			<th colspan="2" scope="colgroup">Action</th>
+		</tr>
 		<tr class="top">
 			<th>No</th>
 			<th>Id</th>
@@ -33,15 +38,24 @@ $no = 1;$data = mysqli_query($conn,"select * from link");while($d = mysqli_fetch
 			<th>Edit</th>
             <th>Delete</th>
 		</tr>
+
+<!-- php -->
+<?php 
+$no = 1;$data = mysqli_query($conn,"select * from link");
+while($d = mysqli_fetch_array($data)){
+?>
+
 		<tr>
 			<td class="td1"><?php echo $no++; ?></td>
 			<td class="td2"><?php echo $d['id']; ?></td>
 			<td class="td3"><?php echo $d['name']; ?></td>
 			<td class="td4"><?php echo $d['link']; ?></td>
-			<td class="td5"><a href="edit?id=<?php echo $d['id']; ?>"><div class="edit"><i class="bx bx-edit"></div></i></a></td>
-			<td class="td6"><a href="delete?id=<?php echo $d['id']; ?>"><div class="delete"><i class="bx bx-trash"></div></i></a></td>
+			<td class="td5"><div class="edit"><a href="delete?id=<?php echo $d['id']; ?>"><i class="bx bx-trash">Edit</i></a></div></td>
+			<td class="td6"><div class="delete"><a href="delete?id=<?php echo $d['id']; ?>"><i class="bx bx-trash">Delete</i></a></div></td>
 		</tr>
 <?php } ?>
 	</table>
 </body>
 </html>
+
+
