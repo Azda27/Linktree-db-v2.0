68f08022b21274dfe42a35ea6597799a3aed3feb
Azda45 <hrowtopiagame875236@gmail.com>
2023-02-17 23:57:16 +0800

    m

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ require 'connection.php'
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="dpowjpowjkp.css">
     <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
@@ -31,18 +30,22 @@ require 'connection.php'
     <title>Azda</title>
 </head>
 <body>
+        <!-- Header -->
     <div class="header">Azda</div>
     <br><br><br><br>
+    <!-- php-query -->
     <?php $data = mysqli_query($conn,"select * from link");while($d = mysqli_fetch_array($data)){?>
-        <a target="_blank" class="link" href="<?php echo $d['link'];?>"><?php echo $d['name'];?>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        </a>
-    <br><br><?php }?>
+        
+        <!-- query-result -->
+        <a target="_blank" class="link" href="<?php echo $d['link'];?>"><?php echo $d['name'];?></a>
+    <!-- End-php-query -->
+        <?php }?>
+            <!-- Footer -->
     <h4 class="footer"><p>&copy; Azda 2021 - 2023</p></h4>
 </body>
+</html>
+
+<!-- style -->
 <style>
   .link {
   display: inline-block;
@@ -56,8 +59,46 @@ require 'connection.php'
   animation: fadeInDown;
   animation-duration: 1.7s;
 }
-</style>
+*{
+    padding: 0;
+    list-style: none;
+}
+body{
+    background: linear-gradient(to left , rgb(151, 255, 104), rgb(218, 146, 52), rgb(4, 62, 255));
+}
+.header{
+    text-align: center;
+    font-size: 42pt;
+    font-family: 'Kdam Thmor Pro', sans-serif;
+  color:white;
+}
+.footer{
+    text-align: center;
+    font-size: 14pt;
+    font-family: 'Nunito', sans-serif;
+color:white;
 
+}
+body {
+	margin: 0;
+	height: 100vh;
+	display: grid;
+	place-items: center;
+}
+
+a {
+	width: 350px;
+  	height: 40px;
+	text-align: center;
+	position: relative;
+	color: white;
+	text-decoration: none;
+	font: 550 30px consolas;
+	overflow: hidden;
+    background-color:black;
+    font-family: 'Raleway', sans-serif;
+}
+</style>
 
 <!-- script -->
 
